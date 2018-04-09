35e8e3fafeaa3dada5491305029ffa2f75d5e307
NKudryavka <nailandtrain@yahoo.co.jp>
2018-04-10 00:28:54 +0900

    add units(wip), font(wip)

--- js/index.js
+++ js/index.js
@@ -1,12 +1,17 @@
+import AOS from 'aos';
 
 document.addEventListener('DOMContentLoaded', () => {
+  AOS.init({
+    duration: 1000,
+    easing: 'ease-out-cubic'
+  });
   const video = document.getElementById('bg-video');
   video.addEventListener('loadedmetadata',  event => {
     const contentHeight = document.body.clientHeight;
     let scrollFire = 0;
     video.pause()
 
-    let updateVideo = () => {
+    const updateVideo = () => {
       let scallY = contentHeight - window.screen.height;
       let newTime = (window.scrollY / scallY) * video.duration;
       video.currentTime = Math.max(0, Math.min(newTime, video.duration-0.01));
