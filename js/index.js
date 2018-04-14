1955856881e4ec6aa5620b24e0e289a92929c779
NKudryavka <nailandtrain@yahoo.co.jp>
2018-04-14 23:23:58 +0900

    fix: add rnd

--- js/index.js
+++ js/index.js
@@ -1,8 +1,11 @@
 import AOS from 'aos';
 
 document.addEventListener('DOMContentLoaded', () => {
+  let voteButton = document.getElementsByClassName('vote-button')[0];
   if(navigator.userAgent.indexOf('iOS') < 0 && navigator.userAgent.indexOf('Android') < 0) {
-    document.getElementsByClassName('vote-button')[0].remove();
+    voteButton.remove();
+  } else {
+    voteButton.href += Math.floor(Math.random() * 100000000);
   }
 
   AOS.init({
